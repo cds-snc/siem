3cf4155060cd14c15e3e36ea1bea04a29f06af94
Max Neuvians <max.neuvians@cds-snc.ca>
2021-06-03 15:50:52 -0400

    feat: tf_plan

--- aws/ip-geolocation/lambda.tf
+++ aws/ip-geolocation/lambda.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "ip_geolocation" {
   function_name = "ip-geolocation"
 
   package_type = "Image"
-  image_uri    = "${aws_ecr_repository.ip_geolocation.repository_url}:5dadf66461f7e23818635ab638e52c0009328bd4"
+  image_uri    = "${aws_ecr_repository.ip_geolocation.repository_url}:e866937c313ff61f6868bb01b43d03781bda7240"
 
   timeout = 900
 
@@ -22,4 +22,24 @@ resource "aws_lambda_function" "ip_geolocation" {
 resource "aws_cloudwatch_log_group" "ip_geolocation" {
   name              = "/aws/lambda/ip-geolocation"
   retention_in_days = 14
+}
+
+resource "aws_cloudwatch_event_rule" "once_a_day" {
+  name                = "ip-geolocation-cron"
+  description         = "Fires once a day"
+  schedule_expression = "rate(24 hours)"
+}
+
+resource "aws_cloudwatch_event_target" "tigger_lambda_once_a_day" {
+  rule      = aws_cloudwatch_event_rule.once_a_day.name
+  target_id = aws_lambda_function.ip_geolocation.function_name
+  arn       = aws_lambda_function.ip_geolocation.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.ip_geolocation.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.once_a_day.arn
 }
