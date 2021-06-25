e2ba16a1bb8c4f2e65f016b414d0e01768a58b00
Max Neuvians <max.neuvians@cds-snc.ca>
2021-06-25 12:56:07 +0000

    fix: fmt

--- lambdas/loader/tests/siem/test_sf_msk.py
+++ lambdas/loader/tests/siem/test_sf_msk.py
@@ -11,15 +11,15 @@ def test_transform_mysql_object(MockUtils):
     }
     result = sf_msk.transform(data)
     assert result == {
-        '@log_s3key': '/1-12345678-1234-1234-1234-123456789012-1/foo/Broker-1_',
-        'msk_cluster_name': '1',
-        'msk_broker_id': '1',
-        'msk_group_id': 'amazon.msk.canary.group.broker-2',
-        'msk_log_type': 'ConsumerLag',
-        'msk_max_lag': '1',
-        'msk_message': 'ConsumerLag for groupId=amazon.msk.canary.group.broker-2 '
-        'topic=canary : SumLag=3 MaxLag=1 TimeLag=60 (xxxxxxxxxxxx)',
-        'msk_sum_lag': '3',
-        'msk_time_lag': '60',
-        'msk_topic': 'canary',
+        "@log_s3key": "/1-12345678-1234-1234-1234-123456789012-1/foo/Broker-1_",
+        "msk_cluster_name": "1",
+        "msk_broker_id": "1",
+        "msk_group_id": "amazon.msk.canary.group.broker-2",
+        "msk_log_type": "ConsumerLag",
+        "msk_max_lag": "1",
+        "msk_message": "ConsumerLag for groupId=amazon.msk.canary.group.broker-2 "
+        "topic=canary : SumLag=3 MaxLag=1 TimeLag=60 (xxxxxxxxxxxx)",
+        "msk_sum_lag": "3",
+        "msk_time_lag": "60",
+        "msk_topic": "canary",
     }
